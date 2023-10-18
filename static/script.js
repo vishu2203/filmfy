6863aced07dd46896c2073b8deb1a1c7b7e0ce74
Vaibhav Verma <147046247+vishu2203@users.noreply.github.com>
2023-10-18 15:12:11 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,22 +1,118 @@
 const search = () => {
-    let searchcnt = document.getElementById('search-box').value.toUpperCase();
-
-    let cnt = document.getElementById('m');
-
-    let div = cnt.getElementsByTagName('div');
-
-    for(i = 0; i>div.length; i++){
-        let a = div[i].getElementsByTagName('a')[0];
-
-        let textvalue = a.innerHTML;
-
-        if (textvalue.toUpperCase().indexOf(searchcnt) > -1) {
-            div[i].style.display = '';
-        }
-
-        else {
-            div[i].style.display = 'none';
-        }
+    const search_item = document.getElementById('search-box').value.toUpperCase();
+    const items = document.getElementById('m');
+    const box = document.getElementsByClassName('box');
+    const heading = items.getElementsByTagName('p');
+    
+    if (search_item == heading[0].innerHTML.toUpperCase()) {
+        box[0].style.display = 'block';
+        box[1].style.display = 'none';
+        box[2].style.display = 'none';
+        box[3].style.display = 'none';
+        box[4].style.display = 'none';
+        box[5].style.display = 'none';
+        box[6].style.display = 'none';
+        box[7].style.display = 'none';
+        box[8].style.display = 'none';
     }
 
+    else if(search_item == heading[1].innerHTML.toUpperCase()) {
+        box[0].style.display = 'none';
+        box[1].style.display = 'block';
+        box[2].style.display = 'none';
+        box[3].style.display = 'none';
+        box[4].style.display = 'none';
+        box[5].style.display = 'none';
+        box[6].style.display = 'none';
+        box[7].style.display = 'none';
+        box[8].style.display = 'none';
+    }
+
+    else if(search_item == heading[2].innerHTML.toUpperCase()) {
+        box[0].style.display = 'none';
+        box[1].style.display = 'none';
+        box[2].style.display = 'block';
+        box[3].style.display = 'none';
+        box[4].style.display = 'none';
+        box[5].style.display = 'none';
+        box[6].style.display = 'none';
+        box[7].style.display = 'none';
+        box[8].style.display = 'none';
+    }
+
+    else if(search_item == heading[3].innerHTML.toUpperCase()) {
+        box[0].style.display = 'none';
+        box[1].style.display = 'none';
+        box[2].style.display = 'none';
+        box[3].style.display = 'block';
+        box[4].style.display = 'none';
+        box[5].style.display = 'none';
+        box[6].style.display = 'none';
+        box[7].style.display = 'none';
+        box[8].style.display = 'none';
+    }
+
+    else if(search_item == heading[4].innerHTML.toUpperCase()) {
+        box[0].style.display = 'none';
+        box[1].style.display = 'none';
+        box[2].style.display = 'none';
+        box[3].style.display = 'none';
+        box[4].style.display = 'block';
+        box[5].style.display = 'none';
+        box[6].style.display = 'none';
+        box[7].style.display = 'none';
+        box[8].style.display = 'none';
+    }
+
+    else if(search_item == heading[5].innerHTML.toUpperCase()) {
+        box[0].style.display = 'none';
+        box[1].style.display = 'none';
+        box[2].style.display = 'none';
+        box[3].style.display = 'none';
+        box[4].style.display = 'none';
+        box[5].style.display = 'block';
+        box[6].style.display = 'none';
+        box[7].style.display = 'none';
+        box[8].style.display = 'none';
+    }
+
+    else if(search_item == heading[6].innerHTML.toUpperCase()) {
+        box[0].style.display = 'none';
+        box[1].style.display = 'none';
+        box[2].style.display = 'none';
+        box[3].style.display = 'none';
+        box[4].style.display = 'none';
+        box[5].style.display = 'none';
+        box[6].style.display = 'block';
+        box[7].style.display = 'none';
+        box[8].style.display = 'none';
+    }
+
+    else if(search_item == heading[7].innerHTML.toUpperCase()) {
+        box[0].style.display = 'none';
+        box[1].style.display = 'none';
+        box[2].style.display = 'none';
+        box[3].style.display = 'none';
+        box[4].style.display = 'none';
+        box[5].style.display = 'none';
+        box[6].style.display = 'none';
+        box[7].style.display = 'block';
+        box[8].style.display = 'none';
+    }
+
+    else if(search_item == heading[10].innerHTML.toUpperCase()) {
+        box[0].style.display = 'none';
+        box[1].style.display = 'none';
+        box[2].style.display = 'none';
+        box[3].style.display = 'none';
+        box[4].style.display = 'none';
+        box[5].style.display = 'none';
+        box[6].style.display = 'none';
+        box[7].style.display = 'none';
+        box[8].style.display = 'block';
+    }
+
+    else {
+        
+    }
 };
